38cd03d644c7b2665155e9bbc69adbfa77b8cf56
pipi0411 <20a41010217@students.hou.edu.vn>
2026-08-15 01:16:10 +0700

    update

--- Build/BuildWeb.loader.js
+++ Build/BuildWeb.loader.js
@@ -52,7 +52,7 @@ function createUnityInstance(canvas, config, onProgress) {
       preserveDrawingBuffer: false,
       powerPreference: 2,
     },
-    wasmFileSize: 80511735,
+    wasmFileSize: 80512200,
     streamingAssetsUrl: "StreamingAssets",
     downloadProgress: {},
     deinitializers: [],
